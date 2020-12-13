39cd4a21ddce822cea625b4f59bd0b328ea03663
Yuyu <howiehye@163.com>
2020-12-13 16:30:16 +0800

    Bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import (setup, find_packages)
 
 setup(
     name="xzitaao",
-    version="0.0.2",
+    version="0.0.3",
     description="Crawler for xzit aao",
     author='Qualthera',
     author_email='qualthera@163.com',
